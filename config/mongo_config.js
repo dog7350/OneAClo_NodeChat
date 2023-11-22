b8866c3a4770324b6f9e0824226dfb32d58f3411
dog7350 <dog7350@gmail.com>
2023-11-23 06:16:42 +0900

    채팅 서버 의존성 수정

--- config/mongo_config.js
+++ config/mongo_config.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const oacChatLogSchema = new Schema({
                                     site : String, // 대상
                                     img : String, // 프로필 사진
-                                    id : String, // 작성자
+                                    writer : String, // 작성자
                                     nick : String, // 닉네임
                                     ext : String, // 확장자
                                     content : String // 내용
